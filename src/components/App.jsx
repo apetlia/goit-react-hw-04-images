be4bac87a55df7fc6c48f4c5a38d7745d650bd98
Artem Petlia <pak@servers.com>
2023-07-30 23:31:54 +0300

    add loadMore button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,99 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import styled from '@emotion/styled';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { ThreeDots } from 'react-loader-spinner';
+
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import * as Pixabay from 'services/pixabay_api';
+import { Button } from './Button/Button';
+
+export class App extends Component {
+  state = {
+    userSearch: '',
+    searchResult: [],
+    page: 1,
+    showLoadMore: false,
+    isLoading: false,
+  };
+
+  handleSubmit = async ({ userSearch }) => {
+    try {
+      this.setState({ isLoading: true, searchResult: [] });
+
+      const searchResult = await Pixabay.getImages({
+        search: userSearch,
+        page: 1,
+      });
+
+      if (searchResult.length === 0) {
+        toast.info('По вашему запросу ничего не найдено');
+        return;
+      }
+
+      this.setState({
+        userSearch,
+        searchResult,
+        page: 1,
+        showLoadMore: true,
+      });
+    } catch (error) {
+      toast.error('Упс, что-то пошло не так, перезагрузите страницу');
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  handleLoadMore = async () => {
+    try {
+      this.setState({ isLoading: true, showLoadMore: false });
+
+      const nextResult = await Pixabay.getImages({
+        search: this.state.userSearch,
+        page: this.state.page + 1,
+      });
+
+      if (nextResult.length === 0) {
+        toast.info('Больше картинок нет');
+        return;
+      }
+
+      this.setState(prevState => ({
+        page: prevState.page + 1,
+        searchResult: [...prevState.searchResult, ...nextResult],
+        showLoadMore: true,
+      }));
+    } catch (error) {
+      toast.error('Упс, что-то пошло не так, перезагрузите страницу');
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  render() {
+    const { searchResult } = this.state;
+
+    return (
+      <Wrapper>
+        <Searchbar onSubmit={this.handleSubmit} />
+        <ImageGallery items={searchResult} />
+        {this.state.isLoading && (
+          <ThreeDots
+            color="#3f51b5"
+            wrapperStyle={{ justifyContent: 'center' }}
+          />
+        )}
+        {this.state.showLoadMore && <Button onClick={this.handleLoadMore} />}
+        <ToastContainer hideProgressBar autoClose={3000} />
+      </Wrapper>
+    );
+  }
+}
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 16px;
+  padding-bottom: 24px;
+`;
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+
+export const ImageGallery = ({ items }) => {
+  return (
+    <List className="gallery">
+      {items.map(({ id, ...otherProps }) => {
+        return <ImageGalleryItem key={id} item={otherProps} />;
+      })}
+    </List>
+  );
+};
+
+const List = styled.ul`
+  display: grid;
+  max-width: calc(100vw - 48px);
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-gap: 16px;
+  margin-top: 0;
+  margin-bottom: 0;
+  padding: 0;
+  list-style: none;
+  margin-left: auto;
+  margin-right: auto;
+`;
